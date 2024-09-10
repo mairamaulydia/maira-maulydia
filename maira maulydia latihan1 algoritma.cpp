d3f8fd04af0541ce5b5f7cd5b3b408ecf5275af6
mairamaulydia <mairalydia66@gmail.com>
2024-09-10 13:54:06 +0700

    Add files via upload

---
+++ maira maulydia latihan1 algoritma.cpp
@@ -0,0 +1,14 @@
+//dibuat oleh
+//tanggal dibuat
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"STMIK PENUSA";
+	cout<<"ALGORITMA PEMROGRAMAN";
+	cout<<5<<endl;
+	cout<<'A'<<endl;
+	return 0;
+}
