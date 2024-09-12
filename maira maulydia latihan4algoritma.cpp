ae5144b0c4244e8e958e341b5cf8911fdc8fb313
mairamaulydia <mairalydia66@gmail.com>
2024-09-12 14:29:34 +0700

    Add files via upload

---
+++ maira maulydia latihan4algoritma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int luas,alas,tinggi;
+	cout<<"masukkan panjang alas=";
+	cin>>alas;
+	cout<<"masukkan tinggi segitiga=";
+	cin>>tinggi;
+	luas=0.5*alas*tinggi;
+	cout<<"luas segitiga sama kaki="<<luas<<endl;
+	
+	return 0;
+}
