dcf4d75ff1644e17466ce1c4e0eb1a4a4ce7dc4c
mairamaulydia <mairalydia66@gmail.com>
2024-09-27 13:31:05 +0700

    Add files via upload

---
+++ maira maulydia latihan 12 algoritma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i =0;
+	while(i<=5){
+		int j =0;
+		while(j<=5){
+			cout<<"perulangan ke-"<<i<<","<<j<<endl;
+			j++;
+			i++;
+		}
+	}
+	return 0;
+}
