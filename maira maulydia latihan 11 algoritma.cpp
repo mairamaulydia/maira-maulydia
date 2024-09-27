4c690fa8022c716b922d7244c22277f7607a6d93
mairamaulydia <mairalydia66@gmail.com>
2024-09-27 13:30:45 +0700

    Add files via upload

---
+++ maira maulydia latihan 11 algoritma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//for pertama baris
+//for kedua kolom
+//dimensi
+
+int main(){
+	for(int i=0;i<=5;i++){
+		for(int j=0;j<=5; j++){
+			for(int k=0;k<=5; k++){
+				cout<<"perulangan ke-"<<i<<","<<j<<","<<k<<endl;
+			}
+		}
+	}
+	return 0;
+}
